bf17aa1f956a87fa3aa78ef5bddacea05fd6908c
Roman Krasiuk <rokrassyuk@gmail.com>
2020-01-23 15:16:51 -0600

    no need to return promise there

--- src/yaml.ts
+++ src/yaml.ts
@@ -1,10 +1,10 @@
 import { safeLoad, LoadOptions } from 'js-yaml'
 import { Action } from './action'
 
-export async function parse(text: string, opts?: LoadOptions): Promise<Action> {
+export function parse(text: string, opts?: LoadOptions): Action {
 	try {
-		return Promise.resolve(new Action(safeLoad(text, opts)))
+		return new Action(safeLoad(text, opts))
 	} catch (err) {
-		return Promise.reject(new Error(`Failed to parse yaml file: ${err.message}`))
+		throw new Error(`Failed to parse yaml file: ${err.message}`)
 	}
 }
