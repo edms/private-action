5d7687d8cdfeecf9cd6c6c6a1f53d7892f38c4c5
Roman Krasiuk <rokrassyuk@gmail.com>
2020-01-23 18:14:05 -0600

    missed one

--- src/yaml.ts
+++ src/yaml.ts
@@ -1,7 +1,6 @@
 import { safeLoad, LoadOptions } from 'js-yaml'
 import { Action } from './action'
 
-<<<<<<< HEAD
 export function parse(text: string, opts?: LoadOptions): Action {
 	try {
 		return new Action(safeLoad(text, opts))
@@ -9,12 +8,3 @@ export function parse(text: string, opts?: LoadOptions): Action {
 		throw new Error(`Failed to parse yaml file: ${err.message}`)
 	}
 }
-=======
-export function parse(text: string, opts?: LoadOptions): Promise<Action> {
-  try {
-    return Promise.resolve(new Action(safeLoad(text, opts)))
-  } catch (err) {
-    return Promise.reject(new Error(`Failed to parse yaml: ${err.message}`))
-  }
-}
->>>>>>> f74605a92b6132a9c92a6daa5907f37b409da974
